43191d5678193434d19641302ff1a1ee3fb27f03
electric sheep <if22b094@technikum-wien.at>
2024-10-16 17:56:51 +0200

    modified router (parsing)

--- MTCG/MTCG/Routing/Router.cs
+++ MTCG/MTCG/Routing/Router.cs
@@ -4,29 +4,38 @@
     {
         public string Route(string request)
         {
-            if (request.StartsWith("GET /login"))
+            string[] requestParts = request.Split(' ');
+            if (requestParts.Length > 2)
             {
-                return "HTTP/1.1 200 OK\r\n" +
-                       "Content-Type: text/plain\r\n" +
-                       "Content-Length: 5\r\n" +
-                       "\r\n" +
-                       "Login";
-            }else if (request.StartsWith("GET /register"))
+                return "HTTP/1.1 400 Bad Request\r\n\r\n";
+            }
+
+            string method = requestParts[0];
+            string path = requestParts[1];
+
+            if (method == "GET" && path == "/login")
             {
-                return "HTTP/1.1 200 OK\r\n" +
-                       "Content-Type: text/plain\r\n" +
-                       "Content-Length: 8\r\n" +
-                       "\r\n" +
-                       "Register";
+                return "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 5\r\n\r\nLogin";
+            }
+            else if (method == "GET" && path == "/register")
+            {
+                return "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 8\r\n\r\nRegister";
+            }
+            else if (method == "POST" && path == "/login")
+            {
+                //muss noch login logic und so implementieren
+                return "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 9\r\n\r\nLogged In";
+            }
+            else if (method == "POST" && path == "/register")
+            {
+                //und registration logic
+                return "HTTP/1.1 201 Created\r\nContent-Type: text/plain\r\nContent-Length: 10\r\n\r\nRegistered";
             }
             else
             {
-                return "HTTP/1.1 404 Not Found\r\n" +
-                       "Content-Type: text/plain\r\n" +
-                       "Content-Length: 9\r\n" +
-                       "\r\n" +
-                       "Login";
+                return "HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\nContent-Length: 9\r\n\r\nNot Found";
             }
+
         }
     }
 }
